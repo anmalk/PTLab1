10c495630c0a539e055a51fe5408e53c906d7ee2
anmalk <anmalkov11@gmail.com>
2024-12-06 21:49:48 +0300

    fixed main.py

--- src/main.py
+++ src/main.py
@@ -4,15 +4,12 @@ import sys
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
 
-
-
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
     parser.add_argument("-p", dest="path", type=str, required=True,
                         help="Path to datafile")
     args = parser.parse_args(args)
     return args.path
-
 
 def main():
     path = get_path_from_arguments(sys.argv[1:])
@@ -23,7 +20,6 @@ def main():
 
     rating = CalcRating(students).calc()
     print("Rating: ", rating)
-
 
 if __name__ == "__main__":
     main()
