20687ddc49829cb94719f96df1e4922f73492d4e
anmalk <anmalkov11@gmail.com>
2024-12-06 23:26:03 +0300

    fixed

--- src/BadStudent.py
+++ src/BadStudent.py
@@ -9,14 +9,13 @@ class BadStudent:
     def calc(self) -> int:
         count_with_debts = 0
 
-        for key in self.data:  
+        for key in self.data:
             bad_mark_count = 0
-            for subject in self.data[key]:  
-                if subject[1] < 61:  
+            for subject in self.data[key]:
+                if subject[1] < 61:
                     bad_mark_count += 1
-            
+
             if bad_mark_count == 2:
                 count_with_debts += 1
 
         return count_with_debts
-
